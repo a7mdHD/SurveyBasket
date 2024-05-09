ba4ade74ab392feb43d3771ad151c1a8c077bc47
Ahmed Hamed <ahmed.hd1011@gmail.com>
2024-05-09 18:22:33 +0300

    Add Identity

---
+++ SurveyBasket.Api/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using SurveyBasket.Api.Entities;
+
+namespace SurveyBasket.Persistence;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :
+        IdentityDbContext<ApplicationUser>(options)
+{
+    public DbSet<Poll> Polls { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
